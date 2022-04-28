673d7ec37b7d341c8a68685677a9025052e8b39f
Melo70015 <qin.yue1@northeastern.edu>
2022-04-28 19:26:11 +0800

    public

---
+++ src/components/ActivateDeactivate.tsx
@@ -0,0 +1,192 @@
+import { AbstractConnector } from '@web3-react/abstract-connector';
+import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core';
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError
+} from '@web3-react/injected-connector';
+import { MouseEvent, ReactElement, useState } from 'react';
+import styled from 'styled-components';
+import { injected } from '../utils/connectors';
+import { useEagerConnect, useInactiveListener } from '../utils/hooks';
+import { Provider } from '../utils/provider';
+
+type ActivateFunction = (
+  connector: AbstractConnector,
+  onError?: (error: Error) => void,
+  throwErrors?: boolean
+) => Promise<void>;
+
+function getErrorMessage(error: Error): string {
+  let errorMessage: string;
+
+  switch (error.constructor) {
+    case NoEthereumProviderError:
+      errorMessage = `No Ethereum browser extension detected. Please install MetaMask extension.`;
+      break;
+    case UnsupportedChainIdError:
+      errorMessage = `You're connected to an unsupported network.`;
+      break;
+    case UserRejectedRequestError:
+      errorMessage = `Please authorize this website to access your Ethereum account.`;
+      break;
+    default:
+      errorMessage = error.message;
+  }
+
+  return errorMessage;
+}
+
+const StyledActivateDeactivateDiv = styled.div`
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 10px;
+  place-self: center;
+  align-items: center;
+`;
+
+const StyledActivateButton = styled.button`
+  appearance: button;
+  background-color: #3cb5a4;
+  border: solid transparent;
+  border-radius: 16px;
+  border-width: 0 0 4px;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: din-round,sans-serif;
+  font-size: 15px;
+  font-weight: 700;
+  letter-spacing: .8px;
+  line-height: 5px;
+  margin: 0;
+  outline: none;
+  overflow: visible;
+  padding: 13px 16px;
+  text-align: center;
+  text-transform: uppercase;
+  touch-action: manipulation;
+  transform: translateZ(0);
+  transition: filter .2s;
+  user-select: none;
+  -webkit-user-select: none;
+  vertical-align: middle;
+  white-space: nowrap;
+  width: 150px;
+  height: 2rem;
+  border-color: green;
+`;
+
+const StyledDeactivateButton = styled.button`
+  appearance: button;
+  background-color: #3cb5a4;
+  border: solid transparent;
+  border-radius: 16px;
+  border-width: 0 0 4px;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: din-round,sans-serif;
+  font-size: 15px;
+  font-weight: 700;
+  letter-spacing: .8px;
+  line-height: 5px;
+  margin: 0;
+  outline: none;
+  overflow: visible;
+  padding: 13px 16px;
+  text-align: center;
+  text-transform: uppercase;
+  touch-action: manipulation;
+  transform: translateZ(0);
+  transition: filter .2s;
+  user-select: none;
+  -webkit-user-select: none;
+  vertical-align: middle;
+  white-space: nowrap;
+  width: 150px;
+  height: 2rem;
+  border-color: red;
+`;
+
+function Activate(): ReactElement {
+  const context = useWeb3React<Provider>();
+  const { activate, active } = context;
+
+  const [activating, setActivating] = useState<boolean>(false);
+
+  function handleActivate(event: MouseEvent<HTMLButtonElement>): void {
+    event.preventDefault();
+
+    async function _activate(activate: ActivateFunction): Promise<void> {
+      setActivating(true);
+      await activate(injected);
+      setActivating(false);
+    }
+
+    _activate(activate);
+  }
+
+  // handle logic to eagerly connect to the injected ethereum provider, if it exists and has
+  // granted access already
+  const eagerConnectionSuccessful = useEagerConnect();
+
+  // handle logic to connect in reaction to certain events on the injected ethereum provider,
+  // if it exists
+  useInactiveListener(!eagerConnectionSuccessful);
+
+  return (
+    <StyledActivateButton
+      disabled={active}
+      style={{
+        cursor: active ? 'not-allowed' : 'pointer',
+        borderColor: activating ? 'orange' : active ? 'unset' : 'green'
+      }}
+      onClick={handleActivate}
+    >
+      Connect
+    </StyledActivateButton>
+  );
+}
+
+function Deactivate(): ReactElement {
+  const context = useWeb3React<Provider>();
+  const { deactivate, active } = context;
+
+  function handleDeactivate(event: MouseEvent<HTMLButtonElement>): void {
+    event.preventDefault();
+
+    deactivate();
+  }
+
+  return (
+    <StyledDeactivateButton
+      disabled={!active}
+      style={{
+        cursor: active ? 'pointer' : 'not-allowed',
+        borderColor: active ? 'red' : 'unset'
+      }}
+      onClick={handleDeactivate}
+    >
+      Disconnect
+    </StyledDeactivateButton>
+  );
+}
+
+export function ActivateDeactivate(): ReactElement {
+  const context = useWeb3React<Provider>();
+  const { error } = context;
+
+  if (!!error) {
+    window.alert(getErrorMessage(error));
+  }
+
+  return (
+    <StyledActivateDeactivateDiv>
+      <Activate />
+      <Deactivate />
+    </StyledActivateDeactivateDiv>
+  );
+}
